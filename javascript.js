5a225da92942df42094cd69270c8ad23d2268d16
XiaGary <garyxia10@gmail.com>
2024-09-07 01:37:58 +1200

    Built the 16x16 grid using logic
    Created a variable that holds the amount equals to
    16x16, then used a for loop which iterates up to
    the variable, where each iteration creates a new
    div, which is then appended to the container div.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-
+const gridAmount = 16*16;
 const container = document.querySelector('#container');
 
-for(let i = 1; i <= 16; i++){
+for(let i = 1; i <= gridAmount; i++){
     const divCreation = document.createElement('div');
     container.appendChild(divCreation);
 }
