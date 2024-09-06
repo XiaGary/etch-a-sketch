a2e1d961157fe06ba9e26f2e1872e66febee2fd2
XiaGary <garyxia10@gmail.com>
2024-09-07 01:23:17 +1200

    Started the foundation of the Etch-A-Sketch project

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+const container = document.querySelector('#container');
+
+for(let i = 1; i <= 16; i++){
+    const divCreation = document.createElement('div');
+    container.appendChild(divCreation);
+}
+
