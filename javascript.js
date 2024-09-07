ca96b9fcba64c21602d2e54c33e40a7bc4a05628
XiaGary <garyxia10@gmail.com>
2024-09-07 18:36:05 +1200

    Completed Etch-A-Sketch project. Changed the grid size limit to 50 instead of 100, and added a reset button, which resets the grid to a blank 16x16 grid. If user inputs a number greater than 50, a warning pops up below the buttons.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,43 @@
-let gridAmount = 16*16;
 const container = document.querySelector('#container');
 
-function createGrid(){
-    for(let i = 1; i <= gridAmount; i++){
-    const divCreation = document.createElement('div');
-    container.appendChild(divCreation);
-    divCreation.addEventListener("mouseover", function(){
-        divCreation.classList.add("black")
-    })
-    }
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', reset);
+function reset(){
+    container.innerHTML = '';
+    createGrid()
 }
-createGrid()
+
+function createGrid(gridAmount = (16*16)){
+    if(gridAmount > 2500){
+        let error = document.querySelector('#error');
+        error.classList.add("error")
+        error.textContent = "Please enter a number below 50!"
+    } else{
+        for(let i = 1; i <= gridAmount; i++){
+        const div = document.createElement('div');
+        div.classList = 'cell'
+        container.appendChild(div);
+        let cellSize = 320/Math.sqrt(gridAmount);
+        div.style.width = `${cellSize}px`
+        div.style.height = `${cellSize}px`
+        div.addEventListener("mouseover", function(){
+            div.classList.add("black");
+        });
+        }
+        error.textContent = '';
+    }
+    
+}
+createGrid();
+
+const btn = document.querySelector('#btn');
+btn.addEventListener("click", removeBoard);
+
+function removeBoard(){
+    container.innerHTML = '';
+    let userChoice = Number(prompt("Pick a grid size"));
+    let newGridSize = userChoice * userChoice;
+    createGrid(newGridSize)
+}
 
 
